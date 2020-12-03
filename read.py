828f348798a6eaac53311914813f498c0447a129
temilaj <temilaj@hotmail.com>
2020-12-03 15:26:23 -0500

    reading video

--- read.py
+++ read.py
@@ -3,7 +3,25 @@
 import cv2 as cv
 
 img = cv.imread('Photos/cats.jpg')
-cv.imshow('Cats', img)
+# img = cv.imread('Photos/cat_large.jpg')
 
+# cv.imshow('Cats', img)
+
+# # Reading Videos
+# 0 index for webcam or any other primary camera
+# capture = cv.VideoCapture(0)
+capture = cv.VideoCapture('Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    
+    cv.imshow('Video', frame)
+
+    # break out of loop if d is pressed
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
 # wait for a specific delay for a key to be pressed. 
 cv.waitKey(0) 

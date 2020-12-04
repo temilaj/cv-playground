5ed122321c9595ff0c3e086c0503ff626dc0ff1a
temilaj <temilaj@hotmail.com>
2020-12-03 23:36:41 -0500

    color histogram

--- Advanced/histogram.py
+++ Advanced/histogram.py
@@ -1,5 +1,3 @@
-#pylint:disable=no-member
-
 import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,19 +5,52 @@ import numpy as np
 img = cv.imread('../Resources/Photos/cats.jpg')
 cv.imshow('Cats', img)
 
+blank = np.zeros(img.shape[:2], dtype='uint8')
+
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+mask = cv.circle(blank, (img.shape[1]//2,img.shape[0]//2), 100, 255, -1)
+
+masked = cv.bitwise_and(img,img,mask=mask)
+cv.imshow('Mask', masked)
+
 # GRayscale histogram
-gray_hist = cv.calcHist([gray], [0], None, [256], [0,256] )
+# gray_hist = cv.calcHist([gray], [0], None, [256], [0,256] )
+
+# plt.figure()
+# plt.title('Grayscale Histogram')
+# plt.xlabel('Bins')
+# plt.ylabel('# of pixels')
+# plt.plot(gray_hist)
+# plt.xlim([0,256])
+# plt.show()
+
+# compute the histogram only on the mask
+# gray_hist = cv.calcHist([gray], [0], mask, [256], [0,256] )
+
+# plt.figure()
+# plt.title('Grayscale Histogram with mask')
+# plt.xlabel('Bins')
+# plt.ylabel('# of pixels')
+# plt.plot(gray_hist)
+# plt.xlim([0,256])
+# plt.show()
+
+
+# Colour Histogram
 
 plt.figure()
-plt.title('Grayscale Histogram')
+plt.title('Colour Histogram')
 plt.xlabel('Bins')
 plt.ylabel('# of pixels')
-plt.plot(gray_hist)
-plt.xlim([0,256])
-plt.show()
+colors = ('b', 'g', 'r')
+for i,col in enumerate(colors):
+    # hist = cv.calcHist([img], [i], None, [256], [0,256])
+    hist = cv.calcHist([img], [i], mask, [256], [0,256])
+    plt.plot(hist, color=col)
+    plt.xlim([0,256])
 
+plt.show()
 
 cv.waitKey(0)

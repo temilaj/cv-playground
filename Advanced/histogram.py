293b995a1e4d1d9f7e4dc8d25bbd6d9bb6d6879c
temilaj <temilaj@hotmail.com>
2020-12-03 23:29:12 -0500

    gray scale histogram

---
+++ Advanced/histogram.py
@@ -0,0 +1,25 @@
+#pylint:disable=no-member
+
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = cv.imread('../Resources/Photos/cats.jpg')
+cv.imshow('Cats', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# GRayscale histogram
+gray_hist = cv.calcHist([gray], [0], None, [256], [0,256] )
+
+plt.figure()
+plt.title('Grayscale Histogram')
+plt.xlabel('Bins')
+plt.ylabel('# of pixels')
+plt.plot(gray_hist)
+plt.xlim([0,256])
+plt.show()
+
+
+cv.waitKey(0)
